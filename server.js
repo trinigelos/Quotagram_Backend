203c323b4d37e88d676896a7f61e36c6c958c230
trinigelos <trinidadgelos7@gmail.com>
2023-08-02 20:45:53 +0200

    Update Backend

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // server.js
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 const uri = 'mongodb://127.0.0.1:27017/quotagram';
@@ -9,6 +10,9 @@ const User = require('./models/User');
 
 // Middleware to parse incoming request bodies as JSON
 app.use(express.json());
+
+// Middleware to enable CORS
+app.use(cors());
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
